790d4e0ddc7fab89a319e25d6225e8dad5f37ca6
Dmitry-mysterious <dimagust5@gmail.com>
2026-05-07 15:59:53 +0300

    8-е ДЗ по 4-м задачам

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,47 @@
 void main() {
-    System.out.println("Hello, World!");
+    //Задача 1
+    System.out.println("--|| Задача 1 ||--");
+
+    int[] nums = new int[3];
+    nums[0] = 1;
+    nums[1] = 2;
+    nums[2] = 3;
+
+    float[] nums2 = {1.57f, 7.654f, 9.986f};
+
+    double[] nums3 = {2.345d, 7.12345678d, 4.34d, 8.9234d};
+
+    System.out.println();
+
+
+    //Задача 2
+    System.out.println("--|| Задача 2 ||--");
+
+    System.out.println("Массив 1: " + nums[0] + ", " + nums[1] + ", " + nums[2]);
+    System.out.println("Массив 2: " + nums2[0] + ", " + nums2[1] + ", " + nums2[2]);
+    System.out.println("Массив 3: " + nums3[0] + ", " + nums3[1] + ", " + nums3[2] + ", " + nums3[3]);
+
+    System.out.println();
+
+
+    //Задача 3
+    System.out.println("--|| Задача 3 ||--");
+
+    System.out.println("Массив 1: " + nums[2] + ", " + nums[1] + ", " + nums[0]);
+    System.out.println("Массив 2: " + nums2[2] + ", " + nums2[1] + ", " + nums2[0]);
+    System.out.println("Массив 3: " + nums3[3] + ", " + nums3[2] + ", " + nums3[1] + ", " + nums3[0]);
+
+    System.out.println();
+
+
+    //Задача 4
+    System.out.println("--|| Задача 4 ||--");
+
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 != 0) {
+            nums[i] += 1;
+        }
+    }
+
+    System.out.println(Arrays.toString(nums));
 }
